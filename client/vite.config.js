80bded40ce079026d356a286ea48dc6a64ccac31
Dev-packick <a.patrickgnimdou@gmail.com>
2026-06-04 01:14:37 +0200

    feat: ajout Docker et configuration déploiement

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    proxy: {
+    proxy: mode === 'development' ? {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
       }
-    }
+    } : {}
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   }
-})
+}))
